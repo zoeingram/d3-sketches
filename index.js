1f8e743647ab41f8511e46ec88cc4a12d9ba3efc
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-04-17 15:20:41 -0700

    adds css framework

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const router = express.Router();
 const PORT = process.env.PORT || 5000
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  app.use(express.static(path.join(__dirname, 'public/')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  // Routes
+  app.get('/', (req, res) => res.render('pages/index'))
