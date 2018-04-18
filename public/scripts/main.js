e8c372f31e7b8e47a952e6716f5b32e4a4dea12c
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-04-17 18:07:49 -0700

    adds homepage divs + links

---
+++ public/scripts/main.js
@@ -0,0 +1,14 @@
+const sketchNames = ['Bar Chart', 'Scatter Plot', 'Linear Scales'];
+
+
+for (let i = 0; i < sketchNames.length; i++) {
+    let sketchDiv = document.createElement('div');
+    sketchDiv.id = 'sketch_' + i + '';
+    sketchDiv.className = 'block';
+
+    let sketchTitle = document.createElement('a');
+    sketchTitle.innerHTML = sketchNames[i];
+    sketchTitle.href = '/sketches/sketch_' + i + '';
+    sketchDiv.appendChild(sketchTitle);
+    document.getElementById('sketches').appendChild(sketchDiv);
+}
