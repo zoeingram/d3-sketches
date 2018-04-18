bdb823ba554f63dd501230504f3c9e91e4a0e8b8
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-04-17 19:11:53 -0700

    adds square root scale

---
+++ public/scripts/sketches/sketch_3.js
@@ -0,0 +1,77 @@
+const dataset = [
+                [ 5, 20 ],
+                [ 480, 90 ],
+                [ 250, 50 ],
+                [ 100, 33 ],
+                [ 330, 95 ],
+                [ 410, 12 ],
+                [ 475, 44 ],
+                [ 25, 67 ],
+                [ 85, 21 ],
+                [ 220, 88 ],
+                [ 600, 150 ]
+               ];
+const w = 500;
+const h = 300;
+const padding = 40;
+
+const xScale = d3.scaleLinear()
+  .domain([0, d3.max(dataset, function(d) {
+      return d[0];
+    }
+  )])
+  .range([padding, w - padding * 2]);
+
+const yScale = d3.scaleLinear()
+  .domain([0, d3.max(dataset, function(d) {
+      return d[1];
+    }
+  )])
+  .range([h - padding, padding]);
+
+
+const aScale = d3.scaleSqrt()
+   .domain([0, d3.max(dataset, function(d) {
+     return d[1];
+   })])
+   .range([0, 10]);
+
+const svg = d3.select('#sketch_3')
+  .append('svg')
+  .attr('width', w)
+  .attr('height', h);
+
+svg.selectAll('circle')
+  .data(dataset)
+  .enter()
+  .append('circle')
+  .attr('cx', function(d) {
+    return xScale(d[0]);
+  })
+  .attr('cy', function(d) {
+    return yScale(d[1]);
+  })
+  .attr("r", function(d) {
+    return aScale(d[1]);
+  })
+  .attr('fill', function(d) {
+    return 'rgb(66, ' + Math.round(d[0]) + ', 220)';
+  });
+
+svg.selectAll('text')
+  .data(dataset)
+  .enter()
+  .append('text')
+  .text(function(d){
+    return d[0] + ',' + d[1];
+  })
+  .attr('x', function(d){
+    return xScale(d[0]) + 20 ;
+  })
+  .attr('y', function(d){
+    return yScale(d[1]);
+  })
+  .attr('font-family', 'monospace')
+  .attr('font-size', '11px')
+  .attr('fill', 'gray')
+  .attr('text-anchor', 'middle');
